0f9d94dc09c7392b09a6dbbd1d0f389bcb4712db
Igor Stepanov <igorstepanov@hotmail.com>
2021-08-19 02:57:25 +0300

    Загрузка проекта

---
+++ scripts/script.js
@@ -0,0 +1,35 @@
+let popup = document.querySelector('.popup');
+let popupForm = popup.querySelector('.popup__container')
+let openPopupButton  = document.querySelector('.profile__btn-edit');
+let closePopupButton  = document.querySelector('.popup__btn-close');
+let profileName = document.querySelector('.profile__name');
+let profileAbout = document.querySelector('.profile__about');
+let inputName = document.querySelector('[name="profileName"]');
+let inputAbout = document.querySelector('[name="profileAbout"]');
+
+function openPopup() {
+  popup.classList.add('popup_opened');
+  inputName.value = profileName.textContent;
+  inputAbout.value = profileAbout.textContent;
+}
+
+function closePopup() {
+  popup.classList.remove('popup_opened');
+}
+
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+  profileName.textContent = inputName.value;
+  profileAbout.textContent = inputAbout.value;
+  closePopup();
+}
+
+function closePopupBackground (evt) {
+  evt.stopPropagation();
+}
+
+popup.addEventListener('click', closePopup);
+popupForm.addEventListener('click', closePopupBackground);
+popupForm.addEventListener('submit', formSubmitHandler);
+openPopupButton.addEventListener('click', openPopup);
+closePopupButton.addEventListener('click', closePopup);
